7dc1a4250a19678cf395525d0c31ca2f0844e128
Jabari-Finney <48698337+Jabari-Finney@users.noreply.github.com>
2021-07-26 14:54:26 -0400

    Initial project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import SimpleInput from './components/LoginForm';
+import BasicForm from './components/SignupForm';
+
+function App() {
+  
+  return (
+
+    <div className="app">
+      <div>
+      <SimpleInput />
+      </div>
+      <div>
+      <BasicForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
